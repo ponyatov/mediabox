ba05aa1613fc9c6bc225e18fdebab4cbc2ad7473
Dmitry Ponyatov <dponyatov@gmail.com>
2023-07-28 13:49:43 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ config/src/lib.rs
@@ -0,0 +1,5 @@
+#![allow(unused)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
+pub const web: &str = "127.0.0.1:7878";
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,26 @@
+#![allow(unused)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    lib::hello();
+    let listener = TcpListener::bind(config::web).unwrap();
+    println!("http://{}", config::web);
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle(stream);
+    }
+}
+
+fn handle(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
