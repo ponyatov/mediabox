724f6275ca83a445d7d69414c0a1da2489724fe7
dponyatov <dponyatov@tender.local>
2024-06-05 16:35:06 +0400

    Signed-off-by: dponyatov <dponyatov@tender.local>

--- root/etc/profile.d/wayland.sh
+++ root/etc/profile.d/wayland.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # export XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-/var/run}
 # export QT_QPA_PLATFORM=${QT_QPA_PLATFORM:-wayland}
-export XDG_RUNTIME_DIR=/tmp
+mkdir /var
+mkdir /var/run
+chmod 0700 /var/run
+export XDG_RUNTIME_DIR=/var/run
